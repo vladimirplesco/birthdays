783c8e1650292fec51001d4b0a10de949bb79a29
Vladimir Plesco <pleshco@deeplace.md>
2023-07-21 17:46:51 +0300

    soon

--- src/assets/js/birth.js
+++ src/assets/js/birth.js
@@ -7,6 +7,7 @@ class Birth {
     this.text = text;
     this.emptyDay = "В этот день никто не родился";
     this.emptyMonth = "В этом месяце никто не родился";
+    this.emptyWeek = "В ближайшую неделю никто не родился";
     this.list = new List(this.id, this.getOptions(), this.getPersons());
   }
 
@@ -35,6 +36,11 @@ class Birth {
       case "day":
         filtered = persons.filter(this.currentDay);
         if (!filtered.length) filtered = [{ name: this.emptyDay, birth: "" }];
+        break;
+      case "soon":
+        filtered = persons.filter(this.nextWeek);
+        if (!filtered.length) filtered = [{ name: this.emptyWeek, birth: "" }];
+        else filtered = this.sortByBirth(filtered);
         break;
       case "month":
         filtered = persons.filter(this.currentMonth);
@@ -83,12 +89,30 @@ class Birth {
     return birthMonth === nowMonth;
   }
 
+  nextWeek = (person) => {
+    const birthParts = person.birth.split(".");
+    const birthMonth = parseInt(birthParts[1]);
+    const birthDay = parseInt(birthParts[0]);
+    const birthOffset = birthMonth * 100 + birthDay;
+    const nowDate = new Date();
+    const nowMonth = nowDate.getMonth() + 1;
+    const nowDay = nowDate.getDate();
+    const nowOffset = nowMonth * 100 + nowDay;
+    const weekDate = new Date();
+    weekDate.setDate(weekDate.getDate() + 7);
+    const weekMonth = weekDate.getMonth() + 1;
+    const weekDay = weekDate.getDate();
+    const weekOffset = weekMonth * 100 + weekDay;
+    return (birthMonth > nowMonth) && (birthMonth < weekMonth);
+  }
+
 }
 
 fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vT-5j3rZHVbVl3fdH6Up-V_eRkb35Qb6Hev1cY0FQgi6RKGrinIiJdDkBno-XxPHMpKO_3MK6Npwakb/pub?gid=0&single=true&output=csv")
   .then(response => response.text())
   .then(text => {
     const day = new Birth("day", text);
+    const soon = new Birth("soon", text);
     const month = new Birth("month", text);
     const all = new Birth("all", text);
   });
